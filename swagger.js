d4114dda6e1e2ab3977f28b8b0ad80f7d374485e
Juan Pablo Muñoz <jpmunoz1320@gmail.com>
2025-09-29 12:12:31 -0300

    se agrega archivo de prueba swagger_output2.json

--- swagger.js
+++ swagger.js
@@ -1,6 +1,6 @@
 import swaggerAutogen from 'swagger-autogen';
 
-const outputFile = './swagger_output.json';
+const outputFile = './swagger_output2.json';
 const endpointsFiles = ['./src/index.js'];
 
 const doc = {
@@ -8,7 +8,7 @@ const doc = {
         title: 'API de usuarios y Servicios',
         description: 'API para gestionar usuarios y servicios, usando metodos crud, con autenticacion JWT y encriptacion de contraseñas con bcryptjs y base de datos MongoDB',
     },
-    host: 'localhost:27017',
+    host: 'localhost:3000',
     schemes: ['http'],
 }
 
